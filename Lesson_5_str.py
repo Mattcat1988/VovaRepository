2acf1d037011c86fccfc9d14a9ec29e8bb7ef00a
Vladimir Babsushkin <zzzzzzz@zzzz.rer>
2021-07-13 15:27:25 +0700

    new

---
+++ Lesson_5_str.py
@@ -0,0 +1,23 @@
+mystring = "bla bla bla"
+
+name = "mr vAsya pupkin"
+
+
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+print("Privet stroka nomer1\nPrivet stroka nomer2\n\nStroka Nomer3")
+print("Messages:\n\tMessage1\n\tMessage2\n\tMessage3\nEND")
+print("Lowe name: " + name.lower())
+
+a = ".... dadya vasya ...."
+print(a)
+print(a.rstrip())
+print(a.lstrip())
+a = a.strip(".")
+a = a.strip()
+print(a.title())
+Piter = "Pidr"
+print(name.title() + Piter)
+#comment print name title+Piter
