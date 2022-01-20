2a9cbcce576406bd3d9327facdaee5e0c568c282
Vladimir Babsushkin <zzzzzzz@zzzz.rer>
2022-01-20 16:35:57 +0700

    new powershell

--- inventoryscriptV3.0.ps1
+++ inventoryscriptV3.0.ps1
@@ -3,3 +3,6 @@ echo "==============Computer Info========" >> 'C:\Users\programs.txt'
 Get-ComputerInfo >> 'C:\Users\programs.txt'
 echo "==============IP========= adress" >> 'C:\Users\programs.txt'
 ipconfig >> 'C:\Users\programs.txt'
+echo "========Disk Info" >> 'C:\Users\programs.txt'
+Get-PhysicalDisk >> 'C:\Users\programs.txt'
+Get-WmiObject -Class Win32_LogicalDisk
